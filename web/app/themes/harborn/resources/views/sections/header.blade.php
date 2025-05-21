e2d5b9f6fde2f5cf59de944b91f37618458d9440
Bilal2903 <Bilal3036@live.nl>
2025-05-21 14:20:04 +0200

    added: implemented  BEM and polylang

--- web/app/themes/harborn/resources/views/sections/header.blade.php
+++ web/app/themes/harborn/resources/views/sections/header.blade.php
@@ -5,26 +5,48 @@
     </a>
 
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav-primary__list', 'echo' => false]) !!}
     </nav>
 
     <div class="header-actions">
       <div class="search-bar">
-        <form role="search" method="get" class="search-form" action="{{ home_url('/') }}">
-          <label>
-            <button type="submit" class="search-submit"></button>
-            <input type="search" class="search-field" placeholder="Search…" value="{{ get_search_query() }}" name="s" />
-          </label>
-        </form>
+        {!! get_search_form(false) !!}
       </div>
 
-      <div class="language-changer">
-        <a href="#" class="current-lang">NL</a>
-        <ul class="language-options">
-          <li><a href="?lang=en">EN</a></li>
-          <li><a href="?lang=de">DE</a></li>
-        </ul>
-      </div>
+      <div class="language-switcher">
+        @if (function_exists('pll_the_languages'))
+            <ul class="language-switcher__list">
+                @php
+                    $languages = pll_the_languages(['raw' => 1]);
+                    $current_lang = pll_current_language('slug');
+
+                    $other_languages = array_filter($languages, function($lang) use ($current_lang) {
+                        return $lang['slug'] !== $current_lang;
+                    });
+                @endphp
+
+                @if (!empty($current_lang))
+                    <li class="language-switcher__item language-switcher__item--current">
+                        <a href="#" class="language-switcher__link">
+                            {{ strtoupper($current_lang) }}
+                        </a>
+          
+                        @if (!empty($other_languages))
+                            <ul class="language-switcher__dropdown">
+                                @foreach ($other_languages as $lang)
+                                    <li class="language-switcher__dropdown-item">
+                                        <a href="{{ $lang['url'] }}" class="language-switcher__dropdown-link">
+                                            {{ strtoupper($lang['slug']) }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    </li>
+                @endif
+            </ul>
+        @endif
+     </div>
     </div>
   </div>
 </header>

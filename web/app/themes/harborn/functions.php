5761a47c5cda7e08286bc47d1a7a6a040c4405c1
Bilal2903 <Bilal3036@live.nl>
2025-05-24 19:14:31 +0200

    Fix: acf blocks

--- web/app/themes/harborn/functions.php
+++ web/app/themes/harborn/functions.php
@@ -60,3 +60,28 @@ collect(['setup', 'filters'])
             );
         }
     });
+
+
+/**
+ * Register all ACF Blocks
+ */
+function my_acf_blocks_init() {
+    if ( function_exists( 'acf_register_block_type' ) ) {
+        acf_register_block_type( array(
+            'name'              => 'hero-block',
+            'title'             => __( 'Hero Block' ),
+            'description'       => __( 'A customizable section for the top of your page.' ),
+            'render_template'   => 'blocks/hero-block/hero-block.php',
+            'category'          => 'layout',
+            'icon'              => 'align-wide',
+            'keywords'          => array( 'hero', 'banner', 'introduction' ),
+            'supports'          => array(
+                'align'         => true,
+                'mode'          => false,
+                'jsx'           => true,
+            ),
+        ) );
+    }
+}
+add_action( 'acf/init', 'my_acf_blocks_init' );
+?>

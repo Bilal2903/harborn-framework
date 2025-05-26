faaa9fbe5740f7dbae86ef79c1851666b6e07d14
Bilal2903 <Bilal3036@live.nl>
2025-05-26 22:05:03 +0200

    feat: carousel test, status OK.

--- tests/Acceptance/Home/HomePageCest.php
+++ tests/Acceptance/Home/HomePageCest.php
@@ -8,12 +8,13 @@ use Tests\Support\AcceptanceTester;
 
 final class HomePageCest
 {
+    
     public function testHeaderElements(AcceptanceTester $I): void
     {
         $I->amOnPage('/');
         $I->seeHeaderElements();
     }
-
+    
     public function tryToSeeHeroSection(AcceptanceTester $I): void
     {
         $I->amOnPage('/');
@@ -21,6 +22,41 @@ final class HomePageCest
         $I->seeElement('.hero-block__heading');
         $I->seeElement('.hero-block__subheading');
         $I->seeElement('.hero-block__button');
+    }
+
+    public function testCarouselBlockIsVisible(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/');
+        $I->seeElement('.carousel-block');
+        $I->see('Werk', '.carousel-block__title');
+        $I->seeElement('.more-work-link-bg');
+        $I->seeElement('.more-work-link');
+        $I->see('meer werk', '.more-work-link');
+        $I->seeElement('swiper-container.mySwiper');
+        $I->seeElement('.project-card');
+        $I->seeElement('.project-card__image');
+        $I->seeElement('.project-card__overlay');
+        $I->seeElement('.project-card__content');
+        $I->seeElement('.project-card__title');
+    }
+
+    public function testCarouselMoreWorkLink(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/');
+        $I->seeLink('meer werk', '/projecten');
+    }
+
+    // public function testCarouselProjectCardCount(AcceptanceTester $I): void
+    // {
+    //     $I->amOnPage('/');
+    //     $I->seeNumberOfElements('.project-card', [1, null]); 
+    // }
+
+    public function testCarouselSwiperJsLoaded(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/');
+        $I->seeInSource('swiper');
+        $I->seeInSource('swiper-container');
     }
 
     public function testFooterElements(AcceptanceTester $I): void

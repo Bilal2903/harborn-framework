1115d2aa1daa717acb31d39587511fbde4d50f55
Bilal2903 <Bilal3036@live.nl>
2025-05-27 09:34:22 +0200

    Fix: code cleanup

--- web/app/themes/harborn/resources/js/app.js
+++ web/app/themes/harborn/resources/js/app.js
@@ -3,44 +3,4 @@ import.meta.glob([
   '../fonts/**',
 ]);
 
-import Swiper, { FreeMode, Mousewheel } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/mousewheel';
-
-document.addEventListener('DOMContentLoaded', function () {
-  const swiperElement = document.querySelector('.carousel-block__swiper.swiper');
-
-  if (swiperElement) {
-    Swiper.use([FreeMode, Mousewheel]);
-    const swiper = new Swiper(swiperElement, {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      freeMode: true,
-      grabCursor: true,
-      mousewheel: true,
-      breakpoints: {
-        0: { slidesPerView: 1.1, spaceBetween: 10 },
-        768: { slidesPerView: 2.1, spaceBetween: 20 },
-        1024: { slidesPerView: 3, spaceBetween: 30 },
-      },
-    });
-
-    let isDragging = false;
-    swiper.on('slideChangeTransitionStart', function () {
-      isDragging = true;
-    });
-    swiperElement.querySelectorAll('.project-card').forEach(link => {
-      link.addEventListener('click', function(e) {
-        if (isDragging) {
-          e.preventDefault();
-          e.stopImmediatePropagation();
-        }
-        isDragging = false;
-      });
-    });
-    swiper.on('touchEnd', function () {
-      setTimeout(() => { isDragging = false; }, 10);
-    });
-  }
-});
+import './carousel/carousel';

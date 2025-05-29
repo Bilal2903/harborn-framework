8be7e5ca4629a408f73247100dab61956428cc2a
Bilal2903 <Bilal3036@live.nl>
2025-05-29 16:51:52 +0200

    Fix: code cleanup

--- web/app/themes/harborn/functions.php
+++ web/app/themes/harborn/functions.php
@@ -106,15 +106,13 @@ add_action('after_setup_theme', function () {
  * Enqueue GSAP and custom scripts
  */
 function theme_gsap_script(){
-    // The core GSAP library
     wp_enqueue_script( 'gsap-js', 'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js', array(), false, true );
-    // Your animation code file - with gsap.js passed as a dependency
     wp_enqueue_script( 'gsap-custom', get_template_directory_uri() . '/resources/js/carousel/carousel.js', array('gsap-js'), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
 
-// Mega Menu navigatie locatie registreren
+// Mega Menu navigation location registration
 add_action('after_setup_theme', function () {
-    register_nav_menu('mega_menu_navigation', __('Mega Menu Navigatie', 'harborn'));
+    register_nav_menu('mega_menu_navigation', __('Mega Menu Navigation', 'harborn'));
 });
 ?>

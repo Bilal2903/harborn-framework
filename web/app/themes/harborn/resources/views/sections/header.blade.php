8f2067d05a9c0e32087017beee723c9eb94abef4
Bilal2903 <Bilal3036@live.nl>
2025-06-09 15:49:39 +0200

    Feat: made the components responsive

--- web/app/themes/harborn/resources/views/sections/header.blade.php
+++ web/app/themes/harborn/resources/views/sections/header.blade.php
@@ -4,6 +4,12 @@
       <img src="{{ get_theme_file_uri('resources/images/logo-light.svg') }}" alt="{{ $siteName }} logo">
     </a>
 
+    <button class="hamburger-toggle d-desktop-none" aria-label="Open menu">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav-primary__list', 'echo' => false]) !!}
     </nav>
